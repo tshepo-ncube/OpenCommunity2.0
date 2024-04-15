4267e710d4a592d73abf2be11ae351d1c0f0208f
hollynowers <139633850+hollynowers@users.noreply.github.com>
2024-04-15 13:30:39 +0200

    Add files via upload

---
+++ app/Home/page.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import  Sidebar from"../_Components/sidebar";
+
+function Home (){
+
+
+return (
+    <div className="App">
+      <Sidebar/>
+    </div>
+
+  );
+}
+
+export default Home; 
---
+++ app/page.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+        if (email.trim() === '' || password.trim() === '') {
+            setErrorMessage('Please enter both email and password');
+            return;
+        }
+
+        // Simulate successful login
+        // Here, you might make an API call to authenticate the user
+        setLoggedIn(true);
+        setErrorMessage(''); // Clear error message upon successful login
+    };
+
+    return (
+        <div className="login-container">
+            {loggedIn ? (
+                <Link href="/">
+                    <a>Go to Home</a>
+                </Link>
+            ) : (
+                <div className="login-form">
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                    {errorMessage && <p className="error-message">{errorMessage}</p>}
+                    <button onClick={handleLogin}>Login</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Login;
