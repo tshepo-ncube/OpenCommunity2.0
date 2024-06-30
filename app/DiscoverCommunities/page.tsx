36c1931e4dcaa9a1fbadfe338efcf0a0067ff060
chnwin005 <chnwin005@myuct.ac.za>
2024-06-30 20:12:36 +0200

    move create community button to top

--- app/DiscoverCommunities/page.tsx
+++ app/DiscoverCommunities/page.tsx
@@ -101,7 +101,7 @@ const CreateCommunity = () => {
       <div className="flex justify-center mt-16 mb-8 ">
         <button
           onClick={handleOpenPopup}
-          className="fixed bottom-4 right-4 btn bg-openbox-green hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none focus:ring-2 focus:ring-primary-300"
+          className="btn bg-openbox-green hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none focus:ring-2 focus:ring-primary-300"
         >
           + CREATE COMMUNITY
         </button>
