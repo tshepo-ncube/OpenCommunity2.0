82040a2ffc3e631a09693550eb50e52c658dbb48
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2024-07-24 19:37:09 +0200

    Welcome Page

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Le9HnkqS4/Jay8ORHIw/Rz0MjCiFA21UrcrMQM84Si4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zAfhTa40LrtEBJBO0Cm/hzUkIqC7mpVZ3chbywct6qE=\"\n}"
