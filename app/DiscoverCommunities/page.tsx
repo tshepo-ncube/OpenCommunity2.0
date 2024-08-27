fefcfb655546f25283a8273e97c966e72bb48422
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2024-08-27 13:36:43 +0200

    Upload Community Profile, missing logic

--- app/DiscoverCommunities/page.tsx
+++ app/DiscoverCommunities/page.tsx
@@ -23,6 +23,22 @@ const CreateCommunity = () => {
   const [userPhone, setUserPhone] = useState("");
   const [roles, setRoles] = useState({ user: false, admin: false });
 
+  // Image fields
+  const [image, setImage] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  };
+
+  const handleUploadButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+  //
+
   const popupRef = useRef(null);
   const userPopupRef = useRef(null);
 
@@ -47,6 +63,8 @@ const CreateCommunity = () => {
       status,
     };
 
+    //Need to add image to community data to then update th DB
+
     if (editIndex !== null) {
       CommunityDB.updateCommunity(
         { id: submittedData[editIndex].id, ...communityData },
@@ -69,6 +87,7 @@ const CreateCommunity = () => {
     setName("");
     setDescription("");
     setCategory("general");
+    setImage(null); //image  
     setEditIndex(null);
     setPopupOpen(false);
   };
@@ -77,6 +96,7 @@ const CreateCommunity = () => {
     setName(submittedData[index].name);
     setDescription(submittedData[index].description);
     setCategory(submittedData[index].category);
+    setImage(submittedData[index].image); //image
     setEditIndex(index);
     setPopupOpen(true);
   };
@@ -143,7 +163,7 @@ const CreateCommunity = () => {
               + CREATE COMMUNITY
             </button>
           </div>
-
+        
           {isPopupOpen && (
             <div className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-md z-10"></div>
           )}
@@ -151,14 +171,13 @@ const CreateCommunity = () => {
           {isPopupOpen && (
             <div
               ref={popupRef}
-              className="mt-16 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-md shadow-xl z-50 w-11/12 sm:w-3/4 lg:w-2/3 xl:w-1/2 h-3/4 sm:h-auto lg:h-auto"
-            >
+              className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-md shadow-xl z-50 w-11/12 sm:w-3/4 lg:w-2/3 xl:w-1/2 h-auto max-h-full overflow-auto">
+              <h1 className="text-xl font-bold  text-gray-700 tracking-wide mb-4">Create a new community</h1>
               <form className="space-y-4">
                 <div>
                   <label
                     htmlFor="name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                    className="block text-m text-gray-700 text-left font-semibold">
                     Name
                   </label>
                   <input
@@ -173,8 +192,7 @@ const CreateCommunity = () => {
                 <div>
                   <label
                     htmlFor="category"
-                    className="block text-sm font-medium text-gray-700"
-                  >
+                    className="block text-m text-gray-700 text-left font-semibold">
                     Category
                   </label>
                   <select
@@ -196,9 +214,10 @@ const CreateCommunity = () => {
                 <div>
                   <label
                     htmlFor="description"
-                    className="block text-sm font-medium text-gray-700"
+                    className="block text-m text-gray-700 text-left font-semibold"
                   >
-                    Community Description
+                    Community Description 
+                    {/* can we have a word limit for this */}
                   </label>
                   <textarea
                     id="description"
@@ -209,11 +228,38 @@ const CreateCommunity = () => {
                   />
                 </div>
 
+                {/* Add Image here */}
+
+                <div className="flex items-center space-x-4">
+                  <label
+                    htmlFor="image"
+                    className="block text-m text-gray-700 font-semibold"
+                  >
+                    Community Profile Image
+                  </label>
+                  <input
+                    type="file"
+                    id="image"
+                    ref={fileInputRef}
+                    onChange={handleImageUpload}
+                    className="hidden"
+                    accept="image/*"
+                  />
+                  <button
+                    type="button"
+                    onClick={handleUploadButtonClick}
+                    className="p-2 bg-openbox-green hover:bg-hover-obgreen text-white rounded-md"
+                  >
+                    Choose Image
+                  </button>
+                  {image && <p className="mt-2 text-gray-600">Uploaded: {image.name}</p>} 
+                </div>
+
                 <div className="flex justify-end">
                   <button
                     type="button"
                     onClick={(e) => handleFormSubmit(e, "draft")}
-                    className="btn bg-openbox-green hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-4 focus:outline-none focus:ring-2 focus:ring-primary-300"
+                    className="btn bg-gray-500 hover:bg-gray-700 btn text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-4 focus:outline-none focus:ring-2 focus:ring-primary-300"
                   >
                     Save as Draft
                   </button>
