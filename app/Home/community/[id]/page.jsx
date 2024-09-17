ab1d10ab979399a2b1e792d21dd17b693a95b798
Holly Nowers <NWRHOL001@myuct.ac.za>
2024-09-17 19:42:57 +0200

    ux ui

--- app/Home/community/[id]/page.jsx
+++ app/Home/community/[id]/page.jsx
@@ -484,7 +484,7 @@ export default function CommunityPage({ params }) {
                       (isRSVPed(event.id) ? (
                         <Button
                           variant="contained"
-                          color="secondary"
+                          style={{ backgroundColor: "grey" }}
                           className="w-full"
                           onClick={() => handleLeave(event.id)}
                         >
@@ -493,7 +493,7 @@ export default function CommunityPage({ params }) {
                       ) : (
                         <Button
                           variant="contained"
-                          color="primary"
+                          style={{ backgroundColor: "#bcd727" }}
                           className="w-full"
                           onClick={() => handleRSVP(event)}
                         >
