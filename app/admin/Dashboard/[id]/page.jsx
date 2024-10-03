c8c1c826ed07794047a2818632c9f4943974529b
Holly Nowers <NWRHOL001@myuct.ac.za>
2024-10-03 19:19:51 +0200

    reoccuring event form ui and fields

--- app/admin/Dashboard/[id]/page.jsx
+++ app/admin/Dashboard/[id]/page.jsx
@@ -286,10 +286,148 @@ const EventForm = ({ isOpen, onClose, onSubmit, eventData }) => {
           </form>
         ) : (
           <div>
-            {/* Tab 2: Reoccurring Events */}
-            <Typography variant="h5" className="text-center mb-4">
-              Reoccurring Event
-            </Typography>
+            <div>
+              <Typography variant="h5" className="text-center mb-4">
+                Reoccuring Event
+              </Typography>
+              <label
+                htmlFor="eventName"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Event Name
+              </label>
+              <input
+                type="text"
+                name="eventName"
+                id="eventName"
+                value={eventDetails.eventName}
+                onChange={handleChangeEvent}
+                placeholder="Enter event name"
+                className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                required
+              />
+            </div>
+
+            <div className="flex justify-between gap-4">
+              <div className="flex-1">
+                <label
+                  htmlFor="startDateTime"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Start Date & Time
+                </label>
+                <input
+                  type="datetime-local"
+                  name="startDateTime"
+                  id="startDateTime"
+                  value={eventDetails.startDateTime}
+                  onChange={handleChangeEvent}
+                  className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                  required
+                />
+              </div>
+              <div className="flex-1">
+                <label
+                  htmlFor="endDateTime"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  End Date & Time
+                </label>
+                <input
+                  type="datetime-local"
+                  name="endDateTime"
+                  id="endDateTime"
+                  value={eventDetails.endDateTime}
+                  onChange={handleChangeEvent}
+                  className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-between gap-4">
+              <div className="flex-1">
+                <label
+                  htmlFor="rsvpEndDateTime"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  RSVP End Date & Time
+                </label>
+                <input
+                  type="datetime-local"
+                  name="rsvpEndDateTime"
+                  id="rsvpEndDateTime"
+                  value={eventDetails.rsvpEndDateTime}
+                  onChange={handleChangeEvent}
+                  className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="location"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Location
+              </label>
+              <Autocomplete
+                apiKey={"AIzaSyA_nwBxUgw4RTZLvfRpt__cS1DIcYprbQ0"}
+                className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                name="location"
+                id="location"
+                onPlaceSelected={(place) => {
+                  setEventDetails((prevDetails) => ({
+                    ...prevDetails,
+                    location: place.formatted_address,
+                  }));
+                }}
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Event Description
+              </label>
+              <textarea
+                name="description"
+                id="description"
+                value={eventDetails.description}
+                onChange={handleChangeEvent}
+                placeholder="Enter event description"
+                className="mt-1 p-3 border border-gray-300 rounded-md w-full text-lg"
+                required
+              ></textarea>
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                type="button"
+                onClick={generateDescription}
+                className="btn bg-purple-400 hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-4"
+              >
+                Generate Description
+              </button>
+              <button
+                type="button"
+                onClick={handleSaveDraft}
+                className="btn bg-openbox-green hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-4"
+              >
+                Save Draft
+              </button>
+              <button
+                type="submit"
+                onClick={handleSubmitEvent}
+                className="btn bg-openbox-green hover:bg-hover-obgreen text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-4"
+              >
+                Post Event
+              </button>
+            </div>
           </div>
         )}
       </div>
