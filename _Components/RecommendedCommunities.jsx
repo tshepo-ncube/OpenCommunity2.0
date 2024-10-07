7910726f43bbfd7893b1bf59368654dc08dee7e7
Tshepo Ncube <Tshepo.Ncube@bbd.co.za>
2024-10-07 14:17:59 +0200

    commit

--- _Components/RecommendedCommunities.jsx
+++ _Components/RecommendedCommunities.jsx
@@ -46,61 +46,7 @@ export default function RecommendedCommunities() {
               </div>
             </li>
           ))}
-
-          {/* <li>
-            <img src="./facebook.svg" alt="Facebook" />
-          </li>
-          <li>
-            <img src="./disney.svg" alt="Disney" />
-          </li>
-          <li>
-            <img src="./airbnb.svg" alt="Airbnb" />
-          </li>
-          <li>
-            <img src="./apple.svg" alt="Apple" />
-          </li>
-          <li>
-            <img src="./spark.svg" alt="Spark" />
-          </li>
-          <li>
-            <img src="./samsung.svg" alt="Samsung" />
-          </li>
-          <li>
-            <img src="./quora.svg" alt="Quora" />
-          </li>
-          <li>
-            <img src="./sass.svg" alt="Sass" />
-          </li> */}
         </ul>
-        {/* <ul
-            class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-            aria-hidden="true"
-          >
-            <li>
-              <img src="./facebook.svg" alt="Facebook" />
-            </li>
-            <li>
-              <img src="./disney.svg" alt="Disney" />
-            </li>
-            <li>
-              <img src="./airbnb.svg" alt="Airbnb" />
-            </li>
-            <li>
-              <img src="./apple.svg" alt="Apple" />
-            </li>
-            <li>
-              <img src="./spark.svg" alt="Spark" />
-            </li>
-            <li>
-              <img src="./samsung.svg" alt="Samsung" />
-            </li>
-            <li>
-              <img src="./quora.svg" alt="Quora" />
-            </li>
-            <li>
-              <img src="./sass.svg" alt="Sass" />
-            </li>
-          </ul> */}
       </div>
     </>
   );
