f82fd50c0a34ab7d9680d7a98ec82e8c6f388bd1
Tshepo Ncube <Tshepo.Ncube@bbd.co.za>
2024-10-15 18:27:58 +0200

    sds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
+//import daisyui from "daisyui";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -104,6 +104,6 @@ const config: Config = {
     },
   },
 
-  plugins: [daisyui],
+  //plugins: [daisyui],
 };
 export default config;
