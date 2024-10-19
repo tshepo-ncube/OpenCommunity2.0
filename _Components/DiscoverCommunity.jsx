ca108a0fa5cc28faf6f8b8a499ed7bb7c296fb1d
Holly Nowers <NWRHOL001@myuct.ac.za>
2024-10-19 02:17:28 +0200

    search ui done

--- _Components/DiscoverCommunity.jsx
+++ _Components/DiscoverCommunity.jsx
@@ -335,7 +335,7 @@ const DiscoverCommunity = ({ email }) => {
                 id="dropdown-button"
                 onClick={toggleDropdown}
                 type="button"
-                className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 min-w-[120px] h-10 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-e-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 whitespace-nowrap"
+                className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 min-w-[120px] h-11 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-e-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 whitespace-nowrap"
               >
                 {selectedCategory}
                 <svg
